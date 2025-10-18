563e7f602f7b67127a824fc618be6ac97fbb3514
Wojciech Witkowski <witus85@gmail.com>
2025-10-18 18:24:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -22,8 +22,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            format_single_linter_file(path, errors)
-        }
+        format_single_linter_file(path, errors)
         for path, errors in linter_report.items()
     ]
